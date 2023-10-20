2d3ade0d7caf3811ac568fb8f2c9a3151d5132da
Tooba <jtooba70@gmail.com>
2023-10-21 00:17:14 +0500

    Answers for lesson 4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,41 @@
 const express = require("express")
-const { products } = require("./data")
+const { products, people } = require("./data")
+const peopleRouter = require('./routes/people')
 const app = express()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+app.use(logger)
 
 app.use(express.static("./public"))
+app.use("/api/v1/people", peopleRouter);
+// app.get('/', logger, (req, res) => {
+//   res.send("Middleware works")
+// })
 
 app.get('/api/v1/test', (req, res) => {
     res.status(200).json({ message: "It worked!" })
 })
+
+// app.get('/api/v1/people', (req, res) => {
+//   res.status(200).json(people)
+// })
+
+// app.post('/api/v1/people', (req, res) => {
+//   const name = req.body.name
+//   if(!name) {
+//     res.status(400).json({ success: false, message: "Please provide a name" });
+//   }
+//   people.push({ id: people.length, name });
+//   res.status(201).json({ success: true, name });
+// })
 
 app.get('/api/v1/products', (req, res) => {
     res.status(200).json(products)
