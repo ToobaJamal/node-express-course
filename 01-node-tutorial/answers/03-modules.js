33cba8b5716f808342e3feae23ddb46a9a89d3d4
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-10-23 13:58:30 +0500

    Update 03-modules.js

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,6 +1,6 @@
 const { john, peter } = require('./04-names')
 const sayHi = require('./05-utils')
-const data = require('./06-alternative-flavor')
+const data = require('./06-alternate-flavor')
 require('./07-mind-grenade')
 
 sayHi('Susan')
