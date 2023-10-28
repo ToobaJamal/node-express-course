83434d7eb3e6a47a0077d15216166006415bcedb
Tooba <jtooba70@gmail.com>
2023-10-28 21:52:49 +0500

    Answers for lesson 6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,18 +3,20 @@ const app = express()
 const tasksRouter = require('./routes/tasks') // used tasksRouter because it makes more sense to me :)
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 // routes
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
-
 app.use('/api/v1/tasks', tasksRouter)
 
-const port = 3000
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000;
 
 const start = async () => { // async function because connectDB returns a promise
     try {
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,25 +1,45 @@
 const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+const {createCustomError} = require('../errors/custom-error')
 
-const getAllTasks = (req, res) => {
-    res.send("All items")
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+        const tasks = await Task.find({}) // leaving obj empty will get all objects
+        res.status(200).json({ tasks })
+})
 
-const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task })
-}
+const createTask = asyncWrapper(async (req, res) => {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+})
 
-const getTask = (req, res) => {
-    res.json(req.params.id)
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+    const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task) {
+            return next(createCustomError(`No task with id: ${taskID}`, 404))
+        }
+        res.status(200).json({ task })
+})
 
-const updateTask = (req, res) => {
-    res.send("Update task")
-}
+const deleteTask = asyncWrapper(async (req, res, next) => {
+    const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task) {
+            return next(createCustomError(`No task with id: ${taskID}`, 404))
+        }
+        res.status(200).json({ task })
+})
 
-const deleteTask = (req, res) => {
-    res.send("Delete task")
-}
+const updateTask = asyncWrapper(async (req, res, next) => {
+    const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new: true, runValidators: true // return new task body, without this this endpoint will return the old value however, it will be updated in the DB
+        })
+        if(!task) {
+            return next(createCustomError(`No task with id: ${taskID}`, 404))
+        }
+        res.status(200).json({task})
+})
 
 module.exports = {
     getAllTasks,
